dd25a19af1f2d4b8400355998b061192b7caee47
MosasaUnited <mostafa447@hotmail.com>
2024-04-12 16:22:27 +0200

    AddNativeSplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:should_todo/should_todo_app.dart';
 
-void main() {
+Future main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  // Remove Splash Screen After 2 Seconds
+  FlutterNativeSplash.remove();
   runApp(const ShouldToDo());
+}
+
+Future initialization(BuildContext? context) async {
+//  load resources
+  await Future.delayed(const Duration(seconds: 2));
 }
