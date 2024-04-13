894e7ca011b273f6b582dc6c274eaeea2b81f003
MosasaUnited <mostafa447@hotmail.com>
2024-04-13 15:58:00 +0200

    AddBottomNavigationBarAndToggleBetweenItems

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 import '../widgets/mydrawer.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +19,30 @@ class HomeScreen extends StatelessWidget {
       drawer: const MyDrawer(),
       appBar: AppBar(
         title: const Text('Should ToDo'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.amber,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100.0),
+        ),
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.task_alt), label: 'Tasks'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.done_outline), label: 'Done'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.archive_outlined), label: 'Archived'),
+        ],
       ),
       body: const Center(
         child: Text('Home Screen'),
